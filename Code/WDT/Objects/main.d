c1fcef7656e74aed93ee80ff377b9475439f1203
arafa arafa <arafa.nextlevel@gmail.com>
2023-06-17 16:29:03 +0300

    Code

---
+++ Code/WDT/Objects/main.d
@@ -0,0 +1,13 @@
+.\objects\main.o: main.c
+.\objects\main.o: MCAL/WDT/wdt.h
+.\objects\main.o: MCAL/WDT/../../STD_LIBRARIES/bit_math.h
+.\objects\main.o: MCAL/WDT/../../STD_LIBRARIES/STD_TYPES.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Users\Arafa\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123.h
+.\objects\main.o: C:\Users\Arafa\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h
+.\objects\main.o: C:\Users\Arafa\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h
+.\objects\main.o: C:\Users\Arafa\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\main.o: C:\Users\Arafa\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main.o: C:\Users\Arafa\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main.o: C:\Users\Arafa\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\main.o: C:\Users\Arafa\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h
---
+++ Code/WDT/Objects/wdt_driver .d
@@ -0,0 +1,13 @@
+.\objects\wdt_driver .o: MCAL\WDT\wdt_driver .c
+.\objects\wdt_driver .o: MCAL\WDT\wdt.h
+.\objects\wdt_driver .o: MCAL\WDT\../../STD_LIBRARIES/bit_math.h
+.\objects\wdt_driver .o: MCAL\WDT\../../STD_LIBRARIES/STD_TYPES.h
+.\objects\wdt_driver .o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\wdt_driver .o: C:\Users\Arafa\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123.h
+.\objects\wdt_driver .o: C:\Users\Arafa\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h
+.\objects\wdt_driver .o: C:\Users\Arafa\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h
+.\objects\wdt_driver .o: C:\Users\Arafa\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\wdt_driver .o: C:\Users\Arafa\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\wdt_driver .o: C:\Users\Arafa\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\wdt_driver .o: C:\Users\Arafa\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\wdt_driver .o: C:\Users\Arafa\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h
